08b78b09c7d3ccddd2ad556cc6f7ac6dd416ddfc
Ahmad Muhardian <dian@petanikode.com>
2024-03-14 04:14:52 +0000

    add comments

--- 02-fungsi-io/fungsi-output.js
+++ 02-fungsi-io/fungsi-output.js
@@ -1,6 +1,10 @@
+// menampilkan teks ke Console Javascript
 console.log("Program dimulai..");
 console.info("Tampilkan teks di ke elemen H1");
+
+// mengisi teks ke elemen headline <h1>
 headline.innerText = "Latihan Fungsi Output";
+
 console.info("Done, sekarang teks sudah ditampilkan di H1");
 
 // menampilkan output ke document
@@ -15,5 +19,8 @@ console.warn("PERINGATAN", "Menyerang website tanpa izin dari pemiliknya adalah 
 // mencoba console debug
 console.log("Berikut ini hasil console debug");
 console.debug("Isi object dari elemen H1 adalah:", headline);
+
+// 👆 jika console.debug tidak tampil hasilnya di Google Chrome, coba aktifkan mode verbose
+// pada Console JS-nya.
 
 alert("Welcome Sir!");
