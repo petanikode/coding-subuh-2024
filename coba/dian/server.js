ad4d6de8bc5181ea3f3f748ae822fd228465a9a1
Dian <dian@petanikode.com>
2024-03-18 05:34:36 +0800

    add latihan checkout

--- coba/dian/server.js
+++ coba/dian/server.js
@@ -1,11 +1,46 @@
-const http = require("node:http");
-const hostname = "127.0.0.1";
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+
+const andaMember = false;
+
+if(isMember){
+  console.log("Anda dapat diskon!");
+} else if(paymentMethod == 'paylater') {
+  console.log("Anda dapat cashback 🤑");
+} else {
+  console.log("tidak dapat apa-apa");
+}
+
+
+if(inputedUsername === username){
+  if(inputedPassword === password){
+    if(user.role === isAdmin){
+      console.log("Login sebagai admin");
+    }
+    console.log("login sebagai member");
+  } else {
+    console.log("password salah");
+  }
+} else {
+  console.log("username salah")
+}
+
+
+
+if(inputedUsername !== username){
+  console.log("username salah")
+  return;
+}
+
+if(inputedPassword !== password){
+  console.log("password salah");
+  return;
+}
+
+if(user.role === isAdmin){
+  console.log("Login sebagai admin");
+}
+
+console.log("login sebagai member");
+
+
+
